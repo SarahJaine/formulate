9be56ddd6206982b41652959a60a79cbf38f4d51
SarahJaine <sarahjaine.sz@gmail.com>
2016-05-05 15:57:25 -0400

    add click.group and command setup

--- formulate.py
+++ formulate.py
@@ -1,3 +1,15 @@
+import click
 
+
+@click.group()
 def cli():
-    print('hello')
+    pass
+
+
+@cli.command()
+@click.option('--to')
+@click.option('--append')
+def virtual(to, append):
+    '''Generate homebrew formula resources for your virutal environment's \
+    pypi packages and their dependencies'''
+    print('hi')
