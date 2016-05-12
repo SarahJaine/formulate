8da7c9dd57adc5f4bed4d2f8437a0b8b6263b0ad
SarahJaine <sarahjaine.sz@gmail.com>
2016-05-12 12:22:56 -0400

    cleaned up some repetition, fixed confirm action

--- formulate.py
+++ formulate.py
@@ -9,9 +9,9 @@ resource_template = Template("""\tresource "{{ resource }}" do\n\
     \tend""")
 
 
-def get_gz_url(raw_data):
+def get_gz_url(r):
     '''return GNU zipped file url from pypi get response'''
-    url_keys = raw_data.json()['urls']
+    url_keys = r.json()['urls']
     for each in url_keys:
         if each['url'][-3:] == ".gz":
             url = each['url']
@@ -35,31 +35,26 @@ def cli(r):
                     name = requirement[0]
                     version = requirement[1]
 
-                    pypi = requests.get(
+                    pypi_r = requests.get(
                             "https://pypi.python.org/pypi/{0}/{1}/json".format(
                                 name, version))
-                    if pypi.status_code == 200:
-                        url = get_gz_url(pypi)
-                        download_response = requests.get(url)
-                        sha = sha256(download_response.content).hexdigest()
-
-                    else:
-                        pypi = requests.get(
+                    if pypi_r.status_code != 200:
+                        pypi_r = requests.get(
                             "https://pypi.python.org/pypi/{0}/json".format(
                                 name))
-                        if pypi.status_code == 200:
-                            version_latest = pypi.json()['info']['version']
-                            click.confirm("{0} version {1} was not found. \
-                                Would you like to use the latest \
-                                stable release, version {2}, instead?".format(
-                                    name, version, version_latest))
-                            url = get_gz_url(pypi)
-                            download_response = requests.get(url)
-                            sha = sha256(download_response.content).hexdigest()
+                        if pypi_r.status_code == 200:
+                            version_latest = pypi_r.json()['info']['version']
+                            if click.confirm('{0} version {1} was not found. \
+                            Would you like to use the latest stable release, \
+                            version {2}, instead?'.format(
+                                    name, version, version_latest)) is False:
+                                continue
+                    url = get_gz_url(pypi_r)
+                    download_response = requests.get(url)
+                    sha = sha256(download_response.content).hexdigest()
                     resources[name] = {
                         'name': name, 'url': url, 'sha256': sha}
                     for key, sub in resources.items():
-                        # print(key, sub)
                         print(resource_template.render(resource=key,
                               url=sub['url'], sha256=sub['sha256']))
 
